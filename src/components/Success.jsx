e5497bbc02231967f79b7d253fde7ef4039f769d
ceylan-karabay <ceylan_karabay@outlook.com>
2026-03-15 22:05:06 +0300

    form validations added

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -4,11 +4,12 @@ export default function Success() {
   const navigate = useNavigate();
   return (
     <div className="success-container">
-      <h1>Giriş Başarılı!</h1>
+      <div className="success-box">
+      <h1>Giriş Başarılı 🎉</h1>
       <p>Hoş geldiniz! Giriş işleminiz başarıyla tamamlandı.</p>
 
-
     <button className='succes-btn' onClick={() => navigate("/")}>Sign Up</button>
+    </div>
     </div>
   );
 }
